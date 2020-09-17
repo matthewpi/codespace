a12b9718382230518c15fead40473ad728f92952
Matthew Penner <me@matthewp.io>
2020-09-17 17:56:25 -0600

    Idk at this point

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -22,19 +22,19 @@ mv go /usr/local/
 
 # Setup oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-cp -R /root/.oh-my-zsh /home/codespace
-curl https://raw.githubusercontent.com/matthewpi/codespace/master/.devcontainer/.zshrc --silent --output /home/codespace/.zshrc
-chown -R $USER_UID:$USER_GID /home/codespace/.oh-my-zsh /home/codespace/.zshrc
+cp -R /root/.oh-my-zsh /home/$USERNAME
+curl https://raw.githubusercontent.com/matthewpi/codespace/master/.devcontainer/.zshrc --silent --output /home/$USERNAME/.zshrc
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/codespace/.zshrc
 
 # Install fast-syntax-highlighting
-git clone https://github.com/zdharma/fast-syntax-highlighting.git /home/codespace/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+git clone https://github.com/zdharma/fast-syntax-highlighting.git /home/$USERNAME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
 
 # Install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/codespace/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$USERNAME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # Install starship
-mkdir /home/codespace/.config
-curl https://raw.githubusercontent.com/matthewpi/codespace/master/.devcontainer/starship.toml --silent --output /home/codespace/.config/starship.toml
+mkdir /home/$USERNAME/.config
+curl https://raw.githubusercontent.com/matthewpi/codespace/master/.devcontainer/starship.toml --silent --output /home/$USERNAME/.config/starship.toml
 
 wget -O- -q "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz" | tar xzf - -C "/usr/local/bin"
-chown root:root /usr/local/bin/starship
+chown $USER_UID:$USER_GID /usr/local/bin/starship
