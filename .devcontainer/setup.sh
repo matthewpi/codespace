69b0c6b3a21a8017e8f22a77129bc45d649cdf60
Matthew Penner <me@matthewp.io>
2020-09-17 18:14:06 -0600

    *insert meme here*

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # Install golang
 cd /root
-curl https://golang.org/dl/go1.15.2.linux-amd64.tar.gz --silent --output
+curl https://golang.org/dl/go1.15.2.linux-amd64.tar.gz --silent
 tar xzf go1.15.2.linux-amd64.tar.gz
 mv go /usr/local/
 
